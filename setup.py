85757dc857a9b247e91f4dee4adcb72f7d2cf87e
kforest <forest.kevin@gmail.com>
2018-07-07 22:58:44 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='demfor',
+    packages=find_packages(),
+    version='0.0.1',
+    description='A short description of the project.',
+    author='kforest',
+    license='MIT',
+)
